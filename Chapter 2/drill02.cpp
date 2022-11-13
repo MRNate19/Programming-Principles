3b64f186d898d0a21003838cfb6b913343e38948
Nathan Gardner <njgardner90@outlook.com>
2022-11-13 22:06:31 +0000

    added drill02.cpp

---
+++ Chapter 2/drill02.cpp
@@ -0,0 +1,24 @@
+/* A few notes:
+   - The information on stroustrup.com is outdated.
+
+   - When you have found the up to date version (github)
+     then copy the source code into notepad and save as
+     std_lib_facilities.h.
+
+   - If you are using the mingw64 compiler, save the file into
+     the include folder within that directory. You can probably
+     do the same within the Visual Studio directory 
+     too. 
+     C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64
+
+   - This should enable you to include "std_lib_facilities.h".
+*/
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, World!\n";
+
+    return 0;
+}
